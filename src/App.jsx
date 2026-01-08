f07d38e7311ef1d988314721ef52c0d37abc190c
sagnikbera <sagnikberaofficial@gmail.com>
2026-01-08 17:23:52 +0530

    Quiz App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FormProvider } from './context/FormContext';
 import EducationForm from './pages/EducationForm';
+import Quiz from './pages/Quiz';
+import LeaderBoard from './pages/LeaderBoard';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<EducationForm />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/leaderboard" element={<LeaderBoard />} />
         </Routes>
       </BrowserRouter>
     </FormProvider>
---
+++ src/pages/LeaderBoard.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import DataTable from 'react-data-table-component';
+import { useNavigate } from 'react-router-dom';
+
+const LeaderBoard = () => {
+  const [result, setResult] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const res = localStorage.getItem('quiz_result');
+
+    console.log('====================================');
+    console.log(res);
+    console.log('====================================');
+
+    if (res) {
+      setResult(JSON.parse(res));
+    }
+  }, []);
+
+  const clearResult = () => {
+    localStorage.removeItem('quiz_result');
+    localStorage.removeItem('name');
+    setResult([]);
+  };
+
+  const columns = [
+    {
+      name: '#',
+      cell: (row, index) => index + 1,
+      width: '60px',
+    },
+    {
+      name: 'Name',
+      cell: (row) => row.name,
+      sortable: true,
+    },
+    {
+      id: 'score',
+      name: 'Score',
+      selector: (row) => row.score,
+      sortable: true,
+    },
+    {
+      name: 'Date',
+      cell: (row) => new Date(row.date).toLocaleDateString(),
+    },
+  ];
+
+  return (
+    <div className="mx-auto max-w-3xl">
+      <h2 className="mb-6 text-3xl font-semibold">LeaderBoard</h2>
+
+      <DataTable
+        columns={columns}
+        data={result}
+        pagination
+        highlightOnHover
+        defaultSortFieldId="score"
+        defaultSortAsc={false}
+        className="rounded-2xl border"
+      />
+
+      <div>
+        {result.length > 0 && (
+          <button
+            onClick={clearResult}
+            className="rounded bg-black px-6 py-2 text-white"
+          >
+            Clear Result
+          </button>
+        )}
+
+        <button
+          className="absolute top-5 right-10 rounded bg-black px-6 py-2 text-white"
+          onClick={() => navigate('/quiz')}
+        >
+          Quiz
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LeaderBoard;
